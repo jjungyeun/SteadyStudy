8256e274df46771529b02f6f5c54adff8a87a07a
jjungyeun <wjddusdl11@ajou.ac.kr>
2019-08-14 21:42:12 +0900

    /register 구현

--- BE/router/register.js
+++ BE/router/register.js
@@ -24,12 +24,39 @@ router.get('/check/member/:id', function(req, res, next) {
 
 // 닉네임 중복확인
 router.get('/check/nickname/:nickname',function(req, res){
-
+    connection.query('SELECT * from userTbl where nickname = "'+req.params.nickname+'"', function(err, data){
+        if(!err){
+            if(data.length == 0)
+                res.send("pass");
+            else
+                res.send("fail");
+        }
+        else {
+            console.log(err);
+            res.send('error');
+        }
+    });
 });
 
 // 회원가입
 // req.body.(id, nickname, birthday, gender, field, sigupdate, changedaytime, saying)
-router.post('signup',function(req, res){
+router.post('/signup',function(req, res){
+    var changedaytime = req.body.changedaytime.split(':')[0];
+    changedaytime = changedaytime.replace(/0/gi, ""); 
+    var sql = "INSERT into userTBL values ('"+req.body.id+
+        "','"+req.body.nickname+"', '"+req.body.birthday+"','"+req.body.gender+
+        "','"+req.body.field+"', default, "+changedaytime+", '"+req.body.saying+"', default);";
+        
+    console.log(sql);
+    connection.query(sql, function(err, data){
+        if(!err){
+            res.send("success");
+        }
+        else   {
+            res.send(err);
+            console.log(err);
+        }
+    });
 
 });
 
